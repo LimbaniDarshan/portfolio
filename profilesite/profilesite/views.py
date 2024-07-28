ab3083f2123566a25bd9c9c6677be69120bf56c8
LimbaniDarshan <dblimbani145@gmail.com>
2024-07-28 17:30:55 +0530

    updating data

--- profilesite/profilesite/views.py
+++ profilesite/profilesite/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render,redirect
-def index(request):
-    return render(request, 'index.html')
+# from django.shortcuts import render,redirect
+# def index(request):
+#     return render(request, 'index.html')
